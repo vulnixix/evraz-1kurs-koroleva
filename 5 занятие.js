54df04508ca9624b2a9dfdbf98ab2b0c8bd0ed8e
vulnixix <vikoroleva02@gmail.com>
2024-10-28 17:54:18 +0500

    5 pfyznbt

---
+++ 5 занятие.js
@@ -0,0 +1,16 @@
+    let a = 1;
+    let b = 'hkjh'
+    let c = true;
+    let d = null;
+    let t = a;
+    let f = a + t;
+    console.log('проверка')
+    console.log(f);
+    let hello = 'привет';
+    let name = 'вика';
+    let st = (name + hello);
+    console.log(a)
+
+
+    let span = document.getElementById('answer');
+    span.innerText = 5;
