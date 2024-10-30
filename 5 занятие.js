e16d2bbd53370d8603929ecf9763591572cd5ef9
vulnixix <vikoroleva02@gmail.com>
2024-10-30 19:58:31 +0500

    5-6 занятие

--- 5 занятие.js
+++ 5 занятие.js
@@ -1,16 +1,53 @@
-    let a = 1;
-    let b = 'hkjh'
-    let c = true;
-    let d = null;
-    let t = a;
-    let f = a + t;
-    console.log('проверка')
-    console.log(f);
-    let hello = 'привет';
-    let name = 'вика';
-    let st = (name + hello);
-    console.log(a)
+    //let a = 1;
+   // let b = 'hkjh'
+   // let c = true;
+   // let d = null;
+    //let t = a;
+    //let f = a + t;
+   // console.log('проверка')
+    //console.log(f);
+    //let hello = 'привет';
+    //let name = 'вика';
+    //let st = (name + hello);
+    //console.log(f)
 
 
-    let span = document.getElementById('answer');
-    span.innerText = 5;
+    //let span = document.getElementById('answer');
+   // span.innerText = 5;
+
+    // выводит сообщение на экране
+   // alert("hello world")
+
+
+    // prompt - окно для ввода данных (всегда строка)
+    //let name = prompt("укажите ваше имя")
+    //console.log(name)
+
+    //console.log(1);
+   // console.log(name);
+
+
+    // confirm - для выбора ответа
+    //let result = confirm('мы изучаем JS?')
+    //console.log(result)
+
+
+    let nameDOM = prompt('name');
+
+    let name = document.getElementById('имя');
+
+    name.innerHTML = nameDOM;
+
+
+    let lastnameDOM = prompt('lastname');
+    let   lastname = document.getElementById('фамилия');
+    lastname.innerHTML = lastnameDOM;
+
+
+
+
+
+
+
+
+
