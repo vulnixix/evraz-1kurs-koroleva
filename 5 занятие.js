6927a5821e7168a0d389966b08c94b2623aec090
vulnixix <vikoroleva02@gmail.com>
2024-11-06 19:57:40 +0500

    7 занятие

--- 5 занятие.js
+++ 5 занятие.js
@@ -31,20 +31,48 @@
     //let result = confirm('мы изучаем JS?')
     //console.log(result)
 
+    //
+    // let a = prompt('введи а')
+    // a = +a;
+    // let b = prompt('введи b')
+    // b = +b;
+    // console.log(a, b)
 
-    let nameDOM = prompt('name');
+    let nameDOM = document.getElementById("name");
+    let name = prompt('имя');
+    nameDOM.innerText = name;
 
-    let name = document.getElementById('имя');
+    let lastnameDOM = document.getElementById("lastname");
+    let lastname =   prompt('фамилия');
+    lastnameDOM.innerText = lastname;
 
-    name.innerHTML = nameDOM;
+    let middlenameDOM = document.getElementById("middlename");
+    let middlename =   prompt('отчество');
+    middlenameDOM.innerText = middlename;
 
+    let ageDOM = document.getElementById("age");
+    let age =   prompt('возраст');
+    ageDOM.innerText = age;
 
-    let lastnameDOM = prompt('lastname');
-    let   lastname = document.getElementById('фамилия');
-    lastname.innerHTML = lastnameDOM;
+    let schoolDOM = document.getElementById("school");
+    let school =   prompt('школа');
+    schoolDOM.innerText = school;
 
+    let klassDOM = document.getElementById("class");
+    let klass =   prompt('класс');
+    klassDOM.innerText = klass;
 
+    let sportDOM = document.getElementById("sport");
+    let sport =   prompt('спорт');
+    sportDOM.innerText = sport;
 
+    let subjectDOM = document.getElementById("subject");
+    let subject =   prompt('предмет');
+    subjectDOM.innerText = subject;
+
+    colorDOM = document.getElementById("color");
+    let color =   prompt('цвет');
+    colorDOM.innerText = color;
 
 
 
---
+++ 7 занятие.js
@@ -0,0 +1,18 @@
+function clearDiv () {
+    // получаем див для очистки
+    let div = document.getElementById("clear");
+    // чистим все внутри
+    div.innerHTML = "";
+
+}
+
+    function makeRed() {
+        let body = document.getElementById("sdf");
+        body.style.backgroundColor = "red";
+    }
+    function makeBlue() {
+
+    }
+    function makeGreen() {
+
+    }
