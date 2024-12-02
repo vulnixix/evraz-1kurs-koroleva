d17c280798589a2aeffa4ce05e230012b868e485
vulnixix <vikoroleva02@gmail.com>
2024-12-02 17:47:31 +0500

    10

--- 66.js
+++ 66.js
@@ -1,9 +1,31 @@
 function makeAlert() {
 
+    let d1 = document.getElementById('xxx');
     let AlertText = prompt('Укажите номер блока, куда добавить текст')
     console.log(AlertText, 'd' + AlertText)
     let div = document.getElementById('d' + AlertText)
-    div.innerText = AlertText
+    div.innerText = div.innerText + d1.value;
+
+
+
 }
 
 
+
+
+function combine() {
+    let div1 = document.getElementById('d1')
+    let div2 = document.getElementById('d2')
+    let div3 = document.getElementById('d3')
+    let span2 = document.getElementById('span2')
+    span2.innerText = div1.innerText + div2.innerText + div3.innerText;
+}
+
+function clearDiv() {
+    let div1 = document.getElementById('d1');
+    let div2 = document.getElementById('d2');
+    let div3 = document.getElementById('d3');
+    div1.innerHTML = "";
+    div2.innerHTML = "";
+    div3.innerHTML = "";
+}
---
+++ 9 занятие.js
@@ -0,0 +1,18 @@
+    //let a = 15;
+    //let b = 15;
+
+    //if (a>b ) {
+       // console.log("a больше b");
+    //}
+    //else {
+      //  console.log("a равна или меньше b");
+    //}
+
+    //if (a>b ) {
+      //  console.log("a больше b");
+    //}
+    //else if (a<b) {
+      //  console.log("a меньше b")
+    //}
+    //console.log("лог снаружи if")
+
