daf123619bc93086630265e39cb3d7e7ecdbc0b4
vulnixix <vikoroleva02@gmail.com>
2025-02-03 17:57:13 +0500

    10

--- 3.25.js
+++ 3.25.js
@@ -1,19 +1,40 @@
 function add() {
     let addElement = document.getElementById('i');
-    let input = document.getElementById('hh')
-    let vol = document.getElementById('fu')
-    let liElement = document.createElement('li');
+    let input = document.getElementById('hh');
+    let vol = document.getElementById('ru');
 
     if (input.value === '') {
         alert('Укажите имя')
+    } else if (vol.value === '') {
+        alert('укажите количество')
+    } else {
+        for (let i = 0; i < +vol.value; i++) {
+            let liElement = document.createElement('li');
+            liElement.innerText = input.value;
+            addElement.append(liElement);
+        }
     }
-    else {
-        liElement.innerText = input.value;
-        addElement.append(liElement);
+}
 
-    }
+function acdb() {
+    let y = document.getElementById('v');
+    let m = document.getElementById('nn');
+    let nm = document.getElementById('fu');
 
-    if (vol.value  ) {
+
+    if (m.value === '') {
+        alert('укажите город')
+    } else if (nm.value === '') {
+        alert('укажите количество')
+    } else {
+        for (let i = 0; i < + nm.value; i++){
+            let p = document.createElement('li');
+            p.innerText = m.value;
+            y.append(p)
+        }
 
     }
 }
+
+
+
---
+++ 4.25.js
@@ -0,0 +1,3 @@
+let block = document.getElementById('a');
+let a = prompt('1 - голубой блок, 2 - красный блок, 3 - желтый блок, 4 - стоп игра');
+
