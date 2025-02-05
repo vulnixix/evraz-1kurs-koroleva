38775cb281582975df7570d276a5d484dfaa6222
vulnixix <vikoroleva02@gmail.com>
2025-02-05 19:53:41 +0500

    10

--- 4.25.js
+++ 4.25.js
@@ -1,3 +1,2 @@
 let block = document.getElementById('a');
 let a = prompt('1 - голубой блок, 2 - красный блок, 3 - желтый блок, 4 - стоп игра');
-
---
+++ 6.25.js
@@ -0,0 +1,6 @@
+function fivenul() {
+    let a = document.getElementById('q');
+    for (let i = 0; i < 6; i++) {
+
+    }
+}
