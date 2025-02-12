6dd3c68be8149bb3245a7079134d3273169e2384
vulnixix <vikoroleva02@gmail.com>
2025-02-12 19:53:07 +0500

    10

--- 6.25.js
+++ 6.25.js
@@ -1,3 +1,23 @@
 function fivenul() {
-    document.getElementById("output").innerText = '1 2 3 4 5'
+    document.getElementById('output_nul').innerText = '0 1 2 3 4'
+}
+
+function fiveone() {
+    document.getElementById('output_one').innerText = '1 2 3 4 5'
+}
+
+function tennul() {
+    document.getElementById('output_ten_nul').innerText = '0 1 2 3 4 5 6 7 8 9'
+}
+function tenone() {
+    document.getElementById('output_ten_one').innerText = '1 2 3 4 5 6 7 8 9 10'
+}
+function tennultwo() {
+    document.getElementById('output_ten_nul_two').innerText = '0 2 4 6 8 10 12 14 16 18'
+}
+function tenonetwo() {
+    document.getElementById('output_ten_one_two').innerText = '1 3 5 7 9 11 13 15 17 19'
+}
+function twentyred() {
+    document.getElementById('output_twenty_red').innerText ='1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20'
 }
