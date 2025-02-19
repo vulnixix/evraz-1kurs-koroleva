6fdee6a6dfa3bf2661815bd80f715a2b1c7e4de0
vulnixix <vikoroleva02@gmail.com>
2025-02-19 19:52:30 +0500

    10

--- 4.25.js
+++ 4.25.js
@@ -1,2 +1,21 @@
-let block = document.getElementById('a');
-let a = prompt('1 - голубой блок, 2 - красный блок, 3 - желтый блок, 4 - стоп игра');
+let div = document.getElementById('container')
+
+while (true) {
+    let res = prompt('1 - голубой блок, 2 - красный блок, 3 - желтый блок, 4 - стоп игра')
+    if (res === '4') {
+        break;
+    } else {
+        let hh = document.createElement('div');
+
+        if (res === '1') {
+            hh.classList.add('block', 'blue')
+            div.append(hh)
+        } if (res === '2') {
+            hh.classList.add('block', 'red')
+            div.append(hh)
+        } else if (res === '3') {
+            hh.classList.add('block', 'yellow')
+            div.append(hh)
+    }
+        }
+}
--- 5.25.js
+++ 5.25.js
@@ -1,3 +1,49 @@
+let list = []
+
 function o() {
-    alert("укажите значение!")
+    let ad = document.getElementById('ad')
+    let input = document.getElementById('as');
+
+    if (input.value === ''){
+        alert ('укажите значение')
+    }
+    else {
+        list.push(input.value)
+
+        // if (list.length > 1) {
+        //     ad.innerText = ad.innerText + ', ' + input.value;
+        // }
+        // else {
+        //     ad.innerText = input.value;
+        // }
+
+        ad.innerText = list.join(', ');
+    }
+}
+function num() {
+    let ol = document.getElementById('tt');
+    if (ad.innerText === ''){
+        alert('нельзя создать пустой список')
+    } else
+        for (let i = 0; i < list.length; i++) {
+            let liElement = document.createElement('li');
+            liElement.innerText = list[i];
+            ol.append(liElement)
+        }
+
+}
+function unnum() {
+    let ul = document.getElementById('gg');
+    if (ad.innerText === ''){
+        alert('нельзя создать пустой список')
+    } else
+        for (let i = 0; i < list.length; i++) {
+            let liElement = document.createElement('li');
+            liElement.innerText = list[i];
+            ul.append(liElement)
+        }
+}
+function del() {
+    let as = document.getElementById('ad')
+    as.innerHTML = '';
 }
