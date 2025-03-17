7f3eb083376016913ae10383a38c062cf0f9287c
vulnixix <vikoroleva02@gmail.com>
2025-03-17 17:56:44 +0500

    конкурс

--- kartochka.js
+++ kartochka.js
@@ -7,6 +7,15 @@ let inputCount = document.getElementById('count');
 let inputDiscount = document.getElementById('discount');
 let inputDescription = document.getElementById('description');
 let inputPickUpPoint = document.getElementById('pick-up-point');
+let form = document.getElementById('form');
+let buttonAdd = document.getElementById('button-add');
+let buttonEdit = document.getElementById('button-edit');
+let titleAdd = document.getElementById('title-add')
+let titleEdit = document.getElementById('title-edit')
+
+let products = [];
+
+let currentEditProduct = null;
 
 let colors = {
     'white' : 'Белый',
@@ -50,6 +59,42 @@ function addTovar() {
     // находим активный радио-инпут, который выбран
     let discountChoose = document.querySelector('input[name=discount]:checked');
 
+    // достаем все чекбоксы особенностей и генерируем текст
+    let specialsText = '';
+    let specialsValues = [];
+    let specials = document.querySelectorAll('input[name=specials]:checked');
+
+    for (let i = 0; i < specials.length; i++) {
+        specialsValues.push(specials[i].value);
+    }
+
+    let paymentText = '';
+    let paymentValues = [];
+    let payment = document.querySelectorAll('input[name=payment]:checked');
+
+    for (let i = 0; i < payment.length; i++) {
+        paymentValues.push(payment[i].value);
+    }
+
+
+    let product = {
+        name:           inputName.value,
+        category:       selectCategory.value,
+        specials:       specialsValues,
+        payments:       paymentValues,
+        description:    inputDescription.value,
+        discountChoose: discountChoose.value,
+        discount:       inputDiscount.value,
+        price:          inputPrice.value,
+        count:          inputCount.value,
+    };
+    let productIndex = products.push(product) - 1;
+
+    addTovarCard(product, productIndex)
+}
+
+function addTovarCard(tovar, index) {
+    event.preventDefault();
     // создаем контейнер для товара
     let cardTovar = document.createElement('div');
     cardTovar.classList.add('tovar');
@@ -58,53 +103,132 @@ function addTovar() {
     let price = ``;
 
     // проверим, а есть ли в discountChoose.value значение
-    if (discountChoose.value === 'true') {
+    if (tovar.discountChoose === 'true') {
         // inputDiscount
-        let disc = +inputPrice.value * +inputDiscount.value / 100;
-        let newPrice = +inputPrice.value - disc;
+        let disc = +tovar.price * +tovar.discount / 100;
+        let newPrice = +tovar.price - disc;
         price = `<div class="tovar-price">
                     <div>Цена: </div>
                     <div>
-                        <div class="tovar-price-old">${ inputPrice.value } руб.</div>
+                        <div class="tovar-price-old">${ tovar.price } руб.</div>
                         <div>${ newPrice } руб.</div>
                     </div>
-                </div>`
+                </div>`;
     }
     else {
-        price = `<div class="tovar-price">Цена: ${ inputPrice.value } руб.</div>`;
+        price = `<div class="tovar-price">Цена: ${ tovar.price } руб.</div>`;
     }
 
     // достаем все чекбоксы особенностей и генерируем текст
     let specialsText = '';
-    let specials = document.querySelectorAll('input[name=specials]:checked');
 
-    for (let i = 0; i < specials.length; i++) {
-        // console.log(specials[i].value, specialsObj[specials[i].value]);
-        specialsText += ' ' + specialsObj[specials[i].value];
+    for (let i = 0; i < tovar.specials.length; i++) {
+        specialsText += ' ' + specialsObj[tovar.specials[i]];
     }
 
+    // достаем все чекбоксы особенностей и генерируем текст
     let paymentText = '';
-    let payment = document.querySelectorAll('input[name=payment]:checked');
 
-    for (let i = 0; i < payment.length; i++) {
-        // console.log(specials[i].value, specialsObj[specials[i].value]);
-        paymentText += ' ' + paymentObj[payment[i].value];
+    for (let i = 0; i < tovar.payments.length; i++) {
+        paymentText += ' ' + paymentObj[tovar.payments[i]];
     }
 
-
-    let card = `<div class="tovar-name">${ inputName.value }</div>
-            <div class="tovar-category">${ categories[selectCategory.value] }</div>
+    let card = `<div class="tovar-name">${ tovar.name }</div>
+            <div class="tovar-category">${ categories[tovar.category] }</div>
             <div class="tovar-color">${colors[selectColor.value]}</div>
             <div class="tovar-specials">Особенности: ${ specialsText }</div>
-            <div class="tovar-description">Описание: ${ inputDescription.value }</div>
+            <div class="tovar-description">Описание: ${ tovar.description }</div>
             <div class="tovar-pick-up-point">Пункт выдачи: ${ inputPickUpPoint.value }</div>
             <div class="tovar-payment">Оплата: ${ paymentText }</div>
             <div class="tovar-price-count">
                 ${ price } 
-                <div class="count">Количество: ${ inputCount.value } шт.</div>
+                <div class="count">Количество: ${ tovar.count } шт.</div>
             </div>
-            <div class="tovar-close">X</div>`
+            <div class="tovar-close">X</div>
+            <div class="tovar-edit">
+                <button onclick="edit(${ index })">Редактировать</button>
+            </div`;
 
     cardTovar.innerHTML = card;
     listDOM.append(cardTovar);
+
+    form.reset();
+}
+
+function editTovar(productIndex) {
+    currentEditProduct = productIndex;
+
+    buttonAdd.classList.add('hide');
+    buttonEdit.classList.remove('hide');
+    titleAdd.classList.add('hide');
+    titleEdit.classList.remove('hide');
+
+    let product = products[productIndex];
+    inputName.value = product.name;
+    inputPrice.value = product.price;
+    inputCount.value = product.count;
+    inputDescription.value = product.description;
+    selectCategory.value = product.category;
+
+    // ищем нужный инпут радио с нужным value значением, чтобы его отметить как выбранный
+    let radio = document.querySelector(`input[name=discount][value=${ product.discountChoose }]`);
+    if (radio) {
+        radio.checked = true;
+    }
+
+    for (let i = 0; i < product.specials.length; i++) {
+        // ищем нужный инпут чекбокс с нужным value значением, чтобы его отметить как выбранный
+        let specialValue = product.specials[i];
+        let checkbox = document.querySelector(`input[name=specials][value=${ specialValue }]`);
+        if (checkbox) {
+            checkbox.checked = true;
+        }
+    }
+}
+
+// обновление товара в массиве товаров после его редактирования
+function editTovar() {
+
+    buttonAdd.classList.remove('hide');
+    buttonEdit.classList.add('hide');
+    titleAdd.classList.remove('hide');
+    titleEdit.classList.add('hide');
+
+    let product = products[currentEditProduct];
+    currentEditProduct = null;
+
+    // находим активный радио-инпут, который выбран
+    let discountChoose = document.querySelector('input[name=discount]:checked');
+
+    // достаем все чекбоксы особенностей и генерируем текст
+    let specialsValues = [];
+    let specials = document.querySelectorAll('input[name=specials]:checked');
+
+    for (let i = 0; i < specials.length; i++) {
+        specialsValues.push(specials[i].value);
+    }
+
+    product.name = inputName.value;
+    product.category = selectCategory.value;
+    product.specials = specialsValues;
+    product.description = inputDescription.value;
+    product.discountChoose = discountChoose.value;
+    product.discount = inputDiscount.value;
+    product.price = inputPrice.value;
+    product.count = inputCount.value;
+
+    event.preventDefault();
+    form.reset();
+
+    buildAgain();
+}
+
+// полностью очищаем список продуктов и строим его заново, вызывая функцию построения карточки товара
+function buildAgain() {
+    listDOM.innerText = '';
+
+    for (let i = 0; i < products.length; i++) {
+        let product = products[i];
+        addTovarCard(product, i)
+    }
 }
